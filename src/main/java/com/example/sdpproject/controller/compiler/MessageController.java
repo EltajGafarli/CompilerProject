193925ebb4d07a393bebc9542c8388464693f555
EltajGafarli <gafarlieltaj@gmail.com>
2024-05-03 12:37:55 +0400

    bugfix

--- src/main/java/com/example/sdpproject/controller/compiler/MessageController.java
+++ src/main/java/com/example/sdpproject/controller/compiler/MessageController.java
@@ -2,12 +2,15 @@ package com.example.sdpproject.controller.compiler;
 
 import com.example.sdpproject.dto.algorithm.MessageDto;
 import com.example.sdpproject.service.compiler.MessageService;
+import liquibase.license.LicenseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/message")
@@ -46,6 +49,14 @@ public class MessageController {
     public ResponseEntity<MessageDto> getMessageById(@PathVariable long id) {
         return ResponseEntity
                 .ok(messageService.getMessageById(id));
+    }
+
+    @GetMapping(path = "/{conversationId}")
+    public ResponseEntity<List<MessageDto>> getAllMessagesByConversationId(@PathVariable long conversationId) {
+        return ResponseEntity
+                .ok(
+                        messageService.getMessagesByConversationId(conversationId)
+                );
     }
 
 
